0320874742751d0a448c546b5baad190c0e5dabf
VinceOmega <vince.omega@gmail.com>
2017-09-03 14:29:10 -0400

    Made some changes to the font of RRP logo

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,17 @@
-var logoMove = anime({
-	targets: '#landing .row-black .logo',
-	top: '100px',
-	duration: 3000,
-	easing: 'easeInOutQuad'
-});
+window.onload = function(){
+	//animation for logo
+	var logoMove = anime({
+		targets: '#landing .trans-black-white .logo',
+		top: '300px',
+		duration: 6000,
+		easing: 'easeInOutQuad'
+	});
 
-var logo = anime({
-	targets: '#landing .row-black .logo',
-	rotateY: 360,
-	direction: 'alternate',
-	duration: 6000,
-	loop: true
-});
-
+	var logo = anime({
+		targets: '#landing .trans-black-white .logo',
+		rotateY: 360,
+		direction: 'alternate',
+		duration: 6000,
+		loop: true
+	});
+}
