c933bbcc24b9dd9c3579d2252f82777be40ae2d0
VinceOmega <vince.omega@gmail.com>
2017-09-05 22:09:30 -0400

    Click to scroll to next section in place.

--- js/animation.js
+++ js/animation.js
@@ -14,4 +14,23 @@ window.onload = function(){
 		duration: 6000,
 		loop: true
 	});
+
 }
+
+$(window).addEvent('domready', function(){
+
+	var fadeFx = $("arrow-section-one");
+	fadeFx.set('tween',{
+		duration: 12000
+	});
+	fadeFx.tween('opacity', 0, 1);
+
+	$$('.arrow-link').addEvent('click', function(el){
+		el.stop();
+		var scrollTo 	= el.target.href.split("#")[1];
+		var arrowPos	= $(scrollTo).getPosition();
+		var doc 		= $(document.body);
+		var scrollFX 	= new Fx.Scroll(doc).set(arrowPos.x, arrowPos.y - ( document.body.offsetHeight / 1.1 ) );
+	});
+
+});
